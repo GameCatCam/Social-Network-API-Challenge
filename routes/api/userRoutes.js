cef9c35c668fef9a0b9032a39af41aa8c8a02a87
GameCatCam <cameronrawlins11@gmail.com>
2023-11-06 23:13:40 -0500

    server runs

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const {
+    //functions go here from "../../controllers/thoughtsController.js"
+} = require('../../controllers/userController')
+
+// Router logic goes here
+
+module.exports = router
